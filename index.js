1d975384545463473e980da98c6fc68f6088f814
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-05-11 17:24:38 -0500

    Updated so that text sits in center of SVG.

--- index.js
+++ index.js
@@ -1,88 +1,71 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { create } = require('xmlbuilder2');
-const { Triangle, Circle, Square } = require('./lib/shapes.js');
+const {Triangle, Circle, Square} = require('./lib/shapes.js');
 
-// Function for user prompt
-async function promptLogoDetails() {
-  const { text, textColor, shape, shapeColor } = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter logo text (up to three characters)',
-      validate: (input) => {
-        return input.length <= 3 ? true : 'Please enter a logo text up to three characters';
-      },
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter logo text color (either the color keyword or hexadecimal number)',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Select a logo shape',
-      choices: ['Triangle', 'Circle', 'Square'],
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter logo shape color (either the color keyword or hexadecimal number)',
-    },
-  ]);
+function generateSVGFile(answers) {
+  const {text, textColor, shape, shapeColor} = answers;
 
-  return { text, textColor, shape, shapeColor };
+  // Create shape object
+  const shapeObject = createShapeObject(shape);
+  // Set color of shape
+  shapeObject.setColor(shapeColor);
+
+  const svgContent = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+      ${shapeObject.render()}
+      <text x="150" y="150" fill="${textColor}" text-anchor="middle" dominant-baseline="middle" font-size="48">${text}</text>
+    </svg>
+  `;
+
+  // Writes SVG content to file
+  fs.writeFileSync('logo.svg', svgContent);
 }
 
-async function generateLogo() {
-  const { text, textColor, shape, shapeColor } = await promptLogoDetails();
-
-  // Create an SVG instance using the xmlbuilder2 library
-  const svg = create({ version: '1.0', encoding: 'UTF-8' })
-    .ele('svg', {
-      xmlns: 'http://www.w3.org/2000/svg',
-      viewBox: '0 0 300 200',
-      width: '300',
-      height: '200',
-    });
-
-  // Add text element
-  svg
-    .ele('text', {
-      x: '10',
-      y: '50',
-      fill: textColor,
-    })
-    .txt(text);
-
-  // Add shape element
-  let shapeInstance;
+// Creates new object
+function createShapeObject(shape) {
   switch (shape) {
-    case 'Triangle':
-      shapeInstance = new Triangle();
-      break;
-    case 'Circle':
-      shapeInstance = new Circle();
-      break;
-    case 'Square':
-      shapeInstance = new Square();
-      break;
+    case 'circle':
+      return new Circle();
+    case 'triangle':
+      return new Triangle();
+    case 'square':
+      return new Square();
+    default:
+      throw new Error('Invalid shape');
   }
-
-  shapeInstance.setColor(shapeColor);
-  const shapeSVGString = shapeInstance.render();
-  const shapeFragment = create(shapeSVGString).node;
-  svg.node.appendChild(shapeFragment.firstChild);
-
-  // Save the logo as a file
-  fs.writeFile('logo.svg', svg.end({ prettyPrint: true }), (err) => {
-    if (err) {
-      console.error('Error saving logo:', err);
-    } else {
-      console.log('Generated logo.svg');
-    }
-  });
 }
 
-generateLogo();
+// User prompts
+const questions = [
+  {
+    name: 'text',
+    type: 'input',
+    message: 'Enter the logo text (up to three characters)',
+    // Validates input
+    validate: function (value) {
+      return value.length <= 3;
+    }
+  },
+  {
+    name: 'textColor',
+    type: 'input',
+    message: 'Enter the logo text color (enter either color keyword or hexadecimal)'
+  },
+  {
+    name: 'shape',
+    type: 'list',
+    message: 'Select a logo shape:',
+    choices: ['Circle', 'Triangle', 'Square']
+  },
+  {
+    name: 'shapeColor',
+    type: 'input',
+    message: 'Enter the logo shape color (enter either the color keyword or hexadecimal):'
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  // Generates SVG files based on prompt inputs
+  generateSVGFile(answers);
+  console.log('Generated logo.svg');
+});
