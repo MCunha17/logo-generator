72009198880aefc80e18696c84b1c4a70019a042
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-05-11 13:40:57 -0500

    Added user prompts.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const inquirer = require('inquirer');
 const {Triangle, Circle, Square} = require('lib/shapes.js');
+
+// Function for user prompt
+async function promptLogoDetails() {
+    const {text, textColor, shape, shapeColor} = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter logo text',
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter logo text color',
+        }
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter logo shape',
+        },
+    ]);
